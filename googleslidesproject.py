f5f56095e38fb8b32b07adf3472a075e4dc84edd
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-06-20 16:06:44 -0400

    google slides project

--- googleslidesproject.py
+++ googleslidesproject.py
@@ -6,8 +6,12 @@ in the Google Slides API.
 """
 #import function setup_googleslides_api
 from quickstart import *
+#import time packages
+import time
+import datetime
 
-#create a new presentation and return  its presentation id
+now = datetime.datetime.now()
+
 def create_presentation(title):
     service = setup_googleslides_api()
     body = { 
@@ -15,8 +19,15 @@ def create_presentation(title):
     }
     presentation = service.presentations().create(body=body).execute()
     presentation_id = presentation.get('presentationId')
-    print(presentation_id)
     return presentation_id
+
+#clients= []
+#presentationids = []
+#for client in clients:
+    #create_presentation(client)
+    #presentationids.append(presentation_id)
+
+
 
 def duplicate_presentation(presentation_id):
     service = setup_googledrive_api()
@@ -30,11 +41,11 @@ def duplicate_presentation(presentation_id):
     return presentation_copy_id
     
     
-def find_replace_slide(presentation_copy_id, before_str, after_str):
+def find_replace_str(slides_id, before_str, after_str):
     service = setup_googleslides_api()
     body =  {
         "requests" : [
-          {
+            {
             "replaceAllText" : {
                 "containsText" : {
                     "text" : before_str,
@@ -46,22 +57,70 @@ def find_replace_slide(presentation_copy_id, before_str, after_str):
 
             }
 
-          }  
-        ] 
-    }
+            }  
+            ] 
+         }
    
-    response = service.presentations().batchUpdate(presentationId = presentation_copy_id, body = body).execute()
-
-
-  
+    response = service.presentations().batchUpdate(presentationId = slides_id, body = body).execute()
 
 
 
+def find_replace_img(slides_id, shape_text, new_img_url):
+    service = setup_googleslides_api()
+    body = {
+        "requests": [
+        {
+            "replaceAllShapesWithImage":{
+            "imageUrl" : new_img_url,
+            "containsText":{
+                "text": shape_text,
+                "matchCase":False
+                 }
+            }
+        }
+    ]
+    }
+    response = service.presentations().batchUpdate(presentationId = slides_id, body = body).execute()
+
+# define current date
+
+# replace "june" with datetime.month function
+#find_replace_slide('17qSfATi1I-0HmQ7LoEgCrz-DkOdw7qt1p4ATg9oika8', '{{mo_3l}}', 'June')
+# replace year with current year
+#find_replace_slide('17qSfATi1I-0HmQ7LoEgCrz-DkOdw7qt1p4ATg9oika8', '{{year_4d}}', '2018')
+# replace with current client's website
+#find_replace_slide('17qSfATi1I-0HmQ7LoEgCrz-DkOdw7qt1p4ATg9oika8', '{{org_website}}', 'www.bbgbroker.com')
     
-    
+#find_replace_img('17qSfATi1I-0HmQ7LoEgCrz-DkOdw7qt1p4ATg9oika8', '{{org_logo}}', 'https://pbs.twimg.com/profile_images/948761950363664385/Fpr2Oz35_400x400.jpg' )
 
 
+#find_replace_str('17qSfATi1I-0HmQ7LoEgCrz-DkOdw7qt1p4ATg9oika8','{{year_4d}}', '{}'.format(now.year) )
 
+find_replace_str('17qSfATi1I-0HmQ7LoEgCrz-DkOdw7qt1p4ATg9oika8', 'six', datetime.date.today().strftime("%B"))
+
+
+clients = ['321 Web Marketing',
+                'BBG',
+                'Beyond Exteriors',
+                'Brown Firm',
+                'Dirt Connections',
+                'Fairfax Christ Lutheran',
+                'FMI',
+                'FVCbank',
+                'Insure My Drone',
+                 'Kangovou',
+                 'KPPB Law',
+                 'MFE Insurance',
+                 'Paw Pals',
+                 'SmartHR',
+                'Comfort Home Care',
+                 'Presidential Heat and Air']
+
+presentationids = []
+for client in clients:
+    client = "{}".format(now.strftime('%m')) + client + " SEO BRIEFING" + now.strftime('%B')+ " {}". format(now.year)
+    presentation_id = create_presentation(client)
+    presentationids.append(presentation_id)
 
 
 
