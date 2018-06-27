1f8b30524a515a99fd7208e2ec1172b6fc7f6c47
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-06-27 15:11:07 -0400

    data table made

--- query.py
+++ query.py
@@ -1,18 +1,14 @@
 from helloanalytics import initialize_analyticsreporting
 import json 
 import pandas 
+months = ['2017-12', '2018-01', '2018-02', '2018-03','2018-04','2018-05','2018-06', '2018-07']
 headers_list = []
 dimensions_list = []
-listtest = []
-months = ['2017-12', '2018-01', '2018-02', '2018-03','2018-04','2018-05','2018-06', '2018-07']
-df = pandas.DataFrame([range(7), range(7), range(7), range(7), range(7)])   
-#view_id_variable
+month_values = []
 view_ids = {}
-#view_ids["www.321webmarketing.com”] = "some_number_from_google_analytics"
 view_ids['www.321webmarketing.com'] = '89636352'
-#view_ids["www.beyondexteriors.com”] = "some_number_from_google_analytics"
 
-#user types in domain name for Google Analytics information
+
 org_name = input('What is the domain name?')
 view_id = view_ids['{}'.format(org_name)]
 
@@ -37,18 +33,20 @@ def get_query(analytics, startdate, enddate):
       ).execute()
 
 def get_metric(response):
+    dict = {}
     for report in response.get('reports', []):
         columnHeader = report.get('columnHeader', {})
-        #dimensionHeaders = columnHeader.get('dimensions', [])
+        dimensionHeaders = columnHeader.get('dimensions', [])
         metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
         for row in report.get('data', {}).get('rows', []):
-            #dimensions = row.get('dimensions', [])
+            dimensions = row.get('dimensions', [])
             dateRangeValues = row.get('metrics', [])
-            #for header, dimension in zip(dimensionHeaders, dimensions):
-                #print(dimension)
+            for header, dimension in zip(dimensionHeaders, dimensions):
+                dict[dimension] = 0
             for i, values in enumerate(dateRangeValues):
                 for metricHeader, value in zip(metricHeaders, values.get('values')):
-                    listtest.append(value)
+                    dict[dimension] =value
+    month_values.append(dict)
                     
 def get_dimension(response):
     for report in response.get('reports', []):
@@ -57,13 +55,10 @@ def get_dimension(response):
         for row in report.get('data', {}).get('rows', []):
             dimensions = row.get('dimensions', [])
             for header, dimension in zip(dimensionHeaders, dimensions):
-                    #for i in range(5):
-                        #data_array[i,0] = dimension
                     dimensions_list.append(dimension)
-    df.index = dimensions_list
 
 
-def monthloops(months):
+def month_loops_for_new_users(months):
     analytics = initialize_analyticsreporting()
     x = get_query(analytics, months[0] + '-01', months[len(months)-1]+ '-31')
     get_dimension(x)
@@ -71,13 +66,56 @@ def monthloops(months):
         headers_list.append(months[i])
         x = get_query(analytics,months[i]+ '-01', months[i+1]+ '-01')
         get_metric(x)
+    
+def make_table(months):
+    Other = []
+    Direct = []
+    Organic_Search = []
+    Referrel = []
+    Social = []
+    df = pandas.DataFrame([range(7), range(7), range(7), range(7), range(7)]) 
+    month_loops_for_new_users(months)
     df.columns = headers_list
+    df.index = dimensions_list
+    for month_value in month_values:
+        try:
+            if month_value['(Other)']:
+                Other.append(month_value['(Other)'])
+            else:
+                Other.append(0)
+        except:
+            Other.append(0)
+        if month_value['Direct']:
+            Direct.append(month_value['Direct'])
+        else:
+            Direct.append(0)
+        if month_value['Organic Search']:
+            Organic_Search.append(month_value['Organic Search'])
+        else:
+            Orangic_Search.append(0)
+        if month_value['Referral']:
+            Referrel.append(month_value['Referral'])
+        else:
+            Referrel.append(0)
+        if month_value['Social']:
+            Social.append(month_value['Social'])
+        else:
+            Social.append(0)
+    df.loc['(Other)', :] = Other
+    df.loc['Direct', : ] = Direct
+    df.loc['Organic Search'] = Organic_Search
+    df.loc['Referral', :] = Referrel
+    df.loc['Social', : ] = Social
+    print(df)
 
-monthloops(months)
+
+
+make_table(months)
+             
+             
 
 
 
-print(df)
-print(listtest)
+
 
 
