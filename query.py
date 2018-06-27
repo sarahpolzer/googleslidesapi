30a11e6812f81e1c63e64288edc03002610e532f
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-06-27 13:45:04 -0400

    A mess

--- query.py
+++ query.py
@@ -1,5 +1,11 @@
 from helloanalytics import initialize_analyticsreporting
 import json 
+import pandas 
+headers_list = []
+dimensions_list = []
+listtest = []
+months = ['2017-12', '2018-01', '2018-02', '2018-03','2018-04','2018-05','2018-06', '2018-07']
+df = pandas.DataFrame([range(7), range(7), range(7), range(7), range(7)])   
 #view_id_variable
 view_ids = {}
 #view_ids["www.321webmarketing.com”] = "some_number_from_google_analytics"
@@ -17,52 +23,61 @@ what_kindof_traffic = input('What kind of traffic? Organic,Social, Other, Direct
 
 
 #query function
-def get_query(analytics):
+def get_query(analytics, startdate, enddate):
     return analytics.reports().batchGet(
       body={
         'reportRequests': [
         {
           'viewId': view_id,
-          'dateRanges': [{'startDate': '365daysAgo', 'endDate': 'today'}],
+          'dateRanges': [{'startDate': startdate, 'endDate': enddate}],
           'metrics': [{'expression':'ga:newUsers'}],
           'dimensions' : [{ 'name' : 'ga:channelGrouping'}],
         }]
       }
       ).execute()
 
-def print_response(response):
+def get_metric(response):
+    for report in response.get('reports', []):
+        columnHeader = report.get('columnHeader', {})
+        #dimensionHeaders = columnHeader.get('dimensions', [])
+        metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+        for row in report.get('data', {}).get('rows', []):
+            #dimensions = row.get('dimensions', [])
+            dateRangeValues = row.get('metrics', [])
+            #for header, dimension in zip(dimensionHeaders, dimensions):
+                #print(dimension)
+            for i, values in enumerate(dateRangeValues):
+                for metricHeader, value in zip(metricHeaders, values.get('values')):
+                    listtest.append(value)
+                    
+def get_dimension(response):
     for report in response.get('reports', []):
         columnHeader = report.get('columnHeader', {})
         dimensionHeaders = columnHeader.get('dimensions', [])
-        metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
         for row in report.get('data', {}).get('rows', []):
             dimensions = row.get('dimensions', [])
-            dateRangeValues = row.get('metrics', [])
             for header, dimension in zip(dimensionHeaders, dimensions):
-                print(header + ': ' + dimension)
-            for i, values in enumerate(dateRangeValues):
-                print('Date range: ' + str(i))
-                for metricHeader, value in zip(metricHeaders, values.get('values')):
-                    print(metricHeader.get('name') + ': ' + value)
+                    #for i in range(5):
+                        #data_array[i,0] = dimension
+                    dimensions_list.append(dimension)
+    df.index = dimensions_list
 
 
-def export_to_json(queryset):
-    with open('query.json', 'w') as outfile:
-        json.dump(queryset, outfile)
-
-    
-#create list of months you are going to loop through
-months = ['201712', '201801', '201802', '201803','201804','201805','201806']
-
-    
-def trial():
+def monthloops(months):
     analytics = initialize_analyticsreporting()
-    queryset = get_query(analytics)
-    print_response(queryset)
+    x = get_query(analytics, months[0] + '-01', months[len(months)-1]+ '-31')
+    get_dimension(x)
+    for i in range(len(months)-1):
+        headers_list.append(months[i])
+        x = get_query(analytics,months[i]+ '-01', months[i+1]+ '-01')
+        get_metric(x)
+    df.columns = headers_list
+
+monthloops(months)
 
 
 
-trial()
-
+print(df)
+print(listtest)
 
 
