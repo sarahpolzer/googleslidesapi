805c56bf77d584a0ee1931e903ba2057de9ee9a8
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-07-11 10:20:39 -0400

    This script is recreating a data table in the reports containing upcoming content for the report month and the following month. This runs and makes a data table in the slides API containing content defined by inputs of the user with a grey header row with black, bold words

--- content_overview.py
+++ content_overview.py
@@ -93,35 +93,114 @@ def edit_google_slides_col_data(table_dict,slides_id, table_Id):
 
 def edit_google_slides_data(table_dict, slides_Id, table_Id):
     service = setup_googleslides_api()
-    data_cells = table_dict['data']
+    rows = table_dict['data']
     cols = table_dict['columns']
-    for item in range(len(data_cells):
-        for col in range(len(data_cells[0]))
+    for row in range(len(rows)):
+        for col in range(len(cols)):
             body = {
                 "requests": [     
                 {
                     "insertText": {
                         "objectId": table_Id,
                         "cellLocation": {
-                            "rowIndex": item+1,
+                            "rowIndex": row+1,
                             "columnIndex": col 
                             },
-                            "text": data_cells['item'][col],
+                            "text": rows[row][col],
                             "insertionIndex": 0
                             }
                         }
                     ]
                 }
             response = service.presentations().batchUpdate(presentationId = slides_id, body = body).execute()
-    
+
+def format_header_row(table_dict, slides_Id, table_Id):
+    service = setup_googleslides_api()
+    cols = table_dict['columns']
+    body = { 
+            "requests": [
+                    {
+                    "updateTableCellProperties": {
+                        "objectId": table_Id,
+                        "tableRange": {
+                        "location": {
+                            "rowIndex": 0,
+                            "columnIndex": 0
+                        },
+                        "rowSpan": 1,
+                        "columnSpan": len(cols)
+                        },
+                        "tableCellProperties": {
+                        "tableCellBackgroundFill": {
+                            "solidFill": {
+                            "color": {
+                                "rgbColor": {
+                                "red": 0.7,
+                                "green": 0.7,
+                                "blue": 0.7
+                                }
+                            }
+                            }
+                        }
+                        },
+                        "fields": "tableCellBackgroundFill.solidFill.color"
+                    }
+                    },
+                ]
+                }
+
+        
+    response = service.presentations().batchUpdate(presentationId = slides_id, body = body).execute()
+
+def format_header_text(table_dict, slides_Id, table_Id):
+    service = setup_googleslides_api()
+    cols = table_dict['columns']
+    for col in range(len(cols)):
+        body = { 
+            "requests": [
+                 {
+                    "updateTextStyle": {
+                        "objectId": table_Id,
+                        "cellLocation": {
+                        "rowIndex": 0,
+                        "columnIndex": col
+                        },
+                        "style": {
+                        "foregroundColor": {
+                            "opaqueColor": {
+                            "rgbColor": {
+                                "red": 0.0,
+                                "green": 0.0,
+                                "blue": 0.0
+                            }
+                            }
+                        },
+                        "bold": True,
+                        "fontFamily": "Cambria",
+                        "fontSize": {
+                            "magnitude": 18,
+                            "unit": "PT"
+                        }
+                        },
+                        "textRange": {
+                        "type": "ALL"
+                        },
+                        "fields": "foregroundColor,bold,fontFamily,fontSize"
+                    }
+                    }, 
+                ]
+            }
+        response = service.presentations().batchUpdate(presentationId = slides_id, body = body).execute()
+
+
 
 def master(slides_id, page_Id, table_Id):
     table_dict = content_posted_next_month()
     create_google_slides_data_table(table_dict, slides_id, page_Id, table_Id)
     edit_google_slides_col_data(table_dict, slides_id, table_Id)
-    
-
-
+    edit_google_slides_data(table_dict, slides_id, table_Id)
+    format_header_row(table_dict, slides_id, table_Id)
+    format_header_text(table_dict, slides_id, table_Id)
 
 slides_id = '17qSfATi1I-0HmQ7LoEgCrz-DkOdw7qt1p4ATg9oika8'
 page_Id = 'g1edf554207_0_7'
