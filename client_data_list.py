7526e6ed80508aa5762875921a65cc37c5b64fb7
Sarah Polzer <sarahpolzer@server-1.fios-router.home.local>
2018-07-23 12:52:50 -0400

    I'm working on getting duplicate slide sets of the template

--- client_data_list.py
+++ client_data_list.py
@@ -5,6 +5,11 @@
 
 import requests
 import json 
+import googleslidesproject
+from googleslidesproject import create_presentation, duplicate_presentation
+
+template_id = '1I5_dqdlr04hLboemHHRniffcMeZ9pc2bSpDffxMLmtE'
+
 
 rm = requests.get('https://my.321webmarketing.com/api/organizations')
 organization_data = json.loads(rm.text)
@@ -20,22 +25,20 @@ what_converts_data = json.loads(Rm.text)
 
 clients = {}
 
+def make_client_dictionary():
+    for item in organization_data:
+        client_information = {}
+        client = item['dba_name']
+        client_information['domain_name'] = item['dba_name']
+        client_information['org_logo'] = item['remote_logo_url']
+        for what_converts in what_converts_data:
+            if str(item['id']) in what_converts['organization']:
+                client_information['what_converts'] = what_converts['account_id']
+        presentation_id = duplicate_presentation(client, template_id)
+        client_information['presentation_id'] = presentation_id
+        clients[client] = client_information
+    with open('client_information.json', 'w') as outfile:
+        return json.dump(clients, outfile)
 
-for item in organization_data:
-    client_information = {}
-    client = item['dba_name']
-    client_information['domain_name'] = item['dba_name']
-    client_information['org_logo'] = item['remote_logo_url']
-    for what_converts in what_converts_data:
-        if str(item['id']) in what_converts['organization']:
-            client_information['what_converts'] = what_converts['account_id']
-    clients[client] = client_information
 
-
-
-
-
-
-    
-with open('client_information.json', 'w') as outfile:
-    json.dump(clients, outfile)
+make_client_dictionary()
