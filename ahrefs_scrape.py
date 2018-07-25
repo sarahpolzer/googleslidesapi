54c74f1108118af3fdc92de5332a9a8c4c66bc37
Sarah Polzer <sarahpolzer@server-1.fios-router.home.local>
2018-07-25 10:29:54 -0400

    yay hoorah I took screenshots

--- ahrefs_scrape.py
+++ ahrefs_scrape.py
@@ -1,32 +1,40 @@
 import os
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.by import By
 
 #don't forget to add your environmental variables
 ahrefs_pw = os.environ['AHREFS_PW']
 ahrefs_un = os.environ['AHREFS_UN']
-
+domain_name = 'kppblaw.com'
 chromedriver = "/usr/local/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 #driver = webdriver.Firefox()
-driver.implicitly_wait(10) # seconds
-driver.get("https://ahrefs.com/user/login/")
-#assert "Ahrefs" in driver.title
-elem = driver.find_element_by_id("email_input")
-elem.send_keys(ahrefs_un)
-elem = driver.find_element_by_name("password")
-elem.send_keys(ahrefs_pw)
-driver.find_element_by_css_selector('input.btn').click()
-sleep(2)
-driver.implicitly_wait(10) # seconds
-driver.get("https://ahrefs.com/site-explorer/overview/v2/subdomains/fresh?target=kppblaw.com")
-sleep(5)
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 8);")
-driver.save_screenshot('domains_count.png')
-driver.find_element_by_xpath("//a[contains(text(), 'Organic search')]").click()
-sleep(15)
-driver.implicitly_wait(10) # seconds
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 6);")
-driver.save_screenshot('keyword_count.png')
-driver.close()
+
+def take_ahrefs_screenshots():
+    driver.implicitly_wait(10) # seconds
+    driver.get("https://ahrefs.com/user/login/")
+    #assert "Ahrefs" in driver.title
+    elem = driver.find_element_by_id("email_input")
+    elem.send_keys(ahrefs_un)
+    elem = driver.find_element_by_name("password")
+    elem.send_keys(ahrefs_pw)
+    driver.find_element_by_css_selector('input.btn').click()
+    sleep(2)
+    driver.implicitly_wait(10) # seconds
+    driver.get("https://ahrefs.com/site-explorer/overview/v2/subdomains/fresh?target=" + domain_name)
+    sleep(5)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 8);")
+    driver.save_screenshot('domains_count.png')
+    driver.find_element_by_xpath('//li[@name="se-overview-tabs"][2]/a').click()
+    sleep(15)
+    driver.implicitly_wait(10) # seconds
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 6);")
+    driver.save_screenshot('keyword_count.png')
+    driver.close()
+    
+
+take_ahrefs_screenshots()
+
+
